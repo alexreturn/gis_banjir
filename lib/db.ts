76f3dde4f2b22b27a5ecf4ece7b2fd54c5cba3a9
i_nyoman_arnawan_x <i_nyoman_arnawan_x@telkomsel.co.id>
2026-03-16 23:28:48 +0800

    fixing port

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: 3306,
+  port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
