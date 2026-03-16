7044dcab6f5b13bdf2b1015940be9c7a6b3bf307
i_nyoman_arnawan_x <i_nyoman_arnawan_x@telkomsel.co.id>
2026-03-16 23:43:41 +0800

    add tls

--- lib/db.ts
+++ lib/db.ts
@@ -8,6 +8,10 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
